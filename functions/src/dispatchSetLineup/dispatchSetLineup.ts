d93bc8e0a0979cb2e94da42cfda07ccdc57b17e1
Graeme Folk <graemefolk@gmail.com>
2023-02-26 14:20:48 -0700

    test out new lineup optimizer for specific users

--- functions/src/dispatchSetLineup/dispatchSetLineup.ts
+++ functions/src/dispatchSetLineup/dispatchSetLineup.ts
@@ -1,5 +1,6 @@
 import { error } from "firebase-functions/logger";
 import { onTaskDispatched } from "firebase-functions/v2/tasks";
+import { setUsersLineup2 } from "./services/newLineupOptimizer.service";
 import { setUsersLineup } from "./services/yahooLineupOptimizer.service";
 
 export const dispatchsetlineup = onTaskDispatched(
@@ -15,6 +16,10 @@ export const dispatchsetlineup = onTaskDispatched(
     },
   },
   async (req) => {
+    const testUsers: string[] = [
+      "RLSrRcWN3lcYbxKQU1FKqditGDu1",
+      "xAyXmaHKO3aRm9J3fnj2rgZRPnX2",
+    ]; // Graeme Folk, Jeff Barnes
     const uid: string = req.data.uid;
     const teams: string[] = req.data.teams;
     if (!uid) {
@@ -27,7 +32,13 @@ export const dispatchsetlineup = onTaskDispatched(
     }
 
     try {
-      return await setUsersLineup(uid, teams);
+      if (testUsers.includes(uid)) {
+        // test out new lineup optimizer for the test users
+        return await setUsersLineup2(uid, teams);
+      } else {
+        // use the old lineup optimizer for everyone else
+        return await setUsersLineup(uid, teams);
+      }
     } catch (err: Error | any) {
       error("Error setting lineup for user " + uid + ". " + err.message);
       console.log("User's teams: " + teams);
