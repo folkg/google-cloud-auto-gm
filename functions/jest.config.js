38d20e311d9cbedf4cde690e43e81428c99405da
Graeme Folk <graemefolk@gmail.com>
2023-04-09 16:45:13 -0600

    skip type-checking for jest to decrease runtime

--- functions/jest.config.js
+++ functions/jest.config.js
@@ -1,6 +1,16 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
+// /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: "ts-jest",
+  transform: {
+    "^.+\\.ts$": [
+      "ts-jest",
+      {
+        isolatedModules: true,
+      },
+    ],
+  },
+  // transform: {
+  //   "^.+\\.ts$": "@swc/jest",
+  // },
   testEnvironment: "node",
   testMatch: ["**/spec/*.spec.ts"],
 };
