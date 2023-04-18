22c91df63dc46f519d5c86bf6b23ef7343eaafed
Graeme Folk <graemefolk@gmail.com>
2023-04-18 12:40:35 -0600

    account for leagues that set on days not Monday

--- functions/src/scheduleSetLineup/scheduleWeeklyLeagueTansactions.ts
+++ functions/src/scheduleSetLineup/scheduleWeeklyLeagueTansactions.ts
@@ -1,9 +1,9 @@
 import { onSchedule } from "firebase-functions/v2/scheduler";
 import { scheduleWeeklyLeagueTransactions } from "./services/scheduleWeeklyLeagueTansactions.service";
 
-// hour 03 = 8pm PST / 9pm PDT Sunday. Somewhat arbitrary, just wanted later Sunday.
+// hour 03 = 8pm PST / 9pm PDT every day. Somewhat arbitrary, just wanted later in the day.
 export const scheduleweeklyleaguetransactions = onSchedule(
-  "0 03 * * MON",
+  "0 03 * * *",
   async () => {
     await scheduleWeeklyLeagueTransactions();
   }
